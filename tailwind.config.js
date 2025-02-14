d8ff61ec243b7c6176605580f280f5b6a6a32cd7
Rosine22 <r.uwineza@alustudent.com>
2025-02-14 10:25:56 +0200

    updates

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        libra: ["Libre Franklin", "Helvetica Neue", Helvetica, Arial, Verdana, sans-serif],
+        GP: [GPC, "Arial Narrow", "Helvetica Neue", Helvetica, Arial, Verdana, sans-serif],
+      },
+    },
+  },
+  plugins: [],
+};
